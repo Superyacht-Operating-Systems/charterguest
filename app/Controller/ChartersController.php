468574e843401f2f115da77ad6eaa5311edaf8de
vignesh <vignesh@ceruleaninfotech.com>
2023-11-30 13:23:24 +0530

    map

--- app/Controller/ChartersController.php
+++ app/Controller/ChartersController.php
@@ -5706,7 +5706,7 @@ class ChartersController extends AppController {
                         $this->set('scheduleData', $scheduleData);
 
                         $this->set('locationimages', $locationimages);
-                        //echo "<pre>";print_r($locationimages); exit;
+                        echo "<pre>";print_r($locationimages); exit;
                         //$this->set('locationComment', $locationComment);
 
                     } else {
